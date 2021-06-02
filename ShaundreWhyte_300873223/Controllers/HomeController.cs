c955fb791f04382bd30526602fb33cec169d63e8
shaundre1 <swhyte18@my.centennialcollege.ca>
2021-06-02 19:35:52 -0400

    Add project files.

---
+++ ShaundreWhyte_300873223/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShaundreWhyte_300873223.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ViewResult Clubs()
+        {
+            return View();
+        }
+        public ViewResult AddClub()
+        {
+            return View();
+        }
+        public ViewResult ClubDetails()
+        {
+            return View();
+        }
+        public ViewResult ManagePlayers()
+        {
+            return View();
+        }
+
+    }
+}
